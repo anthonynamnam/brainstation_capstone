d5fdc43fe4503f3d22e3df1cb1bd9b0170e4ab25
anthonynamnam <kwokanthony073@gmail.com>
2023-11-24 01:42:47 -0800

    first version simple streamlit

---
+++ Streamlit/other_pages/2_👀_Methodology.py
@@ -0,0 +1,75 @@
+# Libraries
+import streamlit as st
+from PIL import Image
+import pathlib
+import logging
+import shutil
+
+# Confit
+st.set_page_config(page_title='Used Car Price Range Prediction', page_icon=':bar_chart:', layout='wide')
+
+# Outlier warning
+st.warning("""
+    The data within this app is no longer being updated. Feel free to check out the same author's
+    new tool called the [Outlier](https://outlier.streamlit.app/) for a more modern UI/UX in
+    presenting the same set of data.
+""")
+
+# Title
+st.title('Project Overview')
+
+st.write(
+    """
+    The crypto industry continues to progress and its development has never stopped. Contributors
+    of each blockchain keep developing each segment of the industry and the whole crypto ecosystem.
+    This tool is designed to allow viewers to journey into the world of crypto ecosystems of some
+    of the major blockchains, and compare their performance.
+
+    This tool is designed and structured in multiple **Pages** that are accessible using the sidebar.
+    Each of these Pages addresses a different segment of the crypto industry. Within each segment
+    (Macro, Transfers, Swaps, NFTs, etc.) you are able to filter your desired blockchains to
+    narrow/expand the comparison. By selecting a single blockchain, you can observe a deep dive
+    into that particular network.
+
+    All values for amounts, prices, and volumes are in **U.S. dollars** and the time frequency of the
+    analysis was limited to the last **30 days**.
+    """
+)
+
+st.subheader('Methodology')
+st.write(
+    """
+    The data for this cross-chain comparison were selected from the [**Flipside Crypto**](https://flipsidecrypto.xyz)
+    data platform by using its **REST API**. These queries are currently set to **re-run every 24 hours** to cover the latest
+    data and are imported as a JSON file directly to each page. The data were selected with a **1 day delay** for all
+    blockchains to be in sync with one another. The codes for this tool are saved and accessible in its 
+    [**GitHub Repository**](https://github.com/alitaslimi/cross-chain-monitoring).
+
+    It is worth mentioning that a considerable portion of the data used for this tool was manually decoded from the raw
+    transaction data on some of the blockchains. Besides that, the names of addresses, DEXs, collections, etc. are also
+    manually labeled. As the queries are updated on a daily basis to cover the most recent data, there is a chance
+    that viewers encounter inconsistent data through the app. Due to the heavy computational power required to execute
+    the queries, and also the size of the raw data being too large, it was not feasible to cover data for a longer period,
+    or by downloading the data and loading it from the repository itself. Therefore, the REST API was selected as the
+    proper form of loading data for the time being.
+    """
+)
+
+st.subheader('Future Works')
+st.write(
+    """
+    This tool is a work in progress and will continue to be developed moving forward. Adding other blockchains,
+    more KPIs and metrics, optimizing the code in general, enhancing the UI/UX of the tool, and more importantly,
+    improving the data pipeline by utilizing [**Flipside ShroomDK**](https://sdk.flipsidecrypto.xyz/shroomdk) are
+    among the top priorities for the development of this app. Feel free to share your feedback, suggestions, and
+    also critics with me.
+    """
+)
+
+c1, c2, c3 = st.columns(3)
+with c1:
+    st.info('**Repo: [This Project](https://github.com/anthonynamnam/brainstation_capstone)**', icon="💡")
+with c2:
+    st.info('**My GitHub: [@anthonynamnam](https://github.com/anthonynamnam)**', icon="💻")
+with c3:
+    st.info('**Data Source: [Kaggle](https://www.kaggle.com/datasets/3ea0a6a45dbd4713a8759988845f1a58038036d84515ded58f65a2ff2bd32e00/data)**', icon="🧠")
